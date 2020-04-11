29a280bccecebaa9e18c37a4d2b3b2b562612151
CharlieDinh <CanhDinh1122>
2020-04-11 23:08:27 +1000

    add server and client model for aplf

---
+++ fedl/servers/serverapfl.py
@@ -0,0 +1,74 @@
+import torch
+import os
+
+from fedl.users.userapfl import UserAPFL
+from fedl.servers.serverbase import Server
+from utils.model_utils import read_data, read_user_data
+import numpy as np
+
+class APFL(Server):
+    def __init__(self, dataset,algorithm, model, batch_size, learning_rate, meta_learning_rate, lamda, num_glob_iters,
+                 local_epochs, optimizer, num_users):
+        super().__init__(dataset,algorithm, model[0], batch_size, learning_rate, meta_learning_rate, lamda, num_glob_iters,
+                         local_epochs, optimizer, num_users)
+
+        # Initialize data for all  users
+        data = read_data(dataset)
+        total_users = len(data[0])
+        for i in range(total_users):
+            id, train , test = read_user_data(i, data, dataset)
+            user = UserAPFL(id, train, test, model, batch_size, learning_rate, meta_learning_rate, lamda, local_epochs, optimizer)
+            self.users.append(user)
+            self.total_train_samples += user.train_samples
+        print("Number of users / total users:",num_users, " / " ,total_users)
+        print("Finished creating Local Descent APFL.")
+
+    def send_grads(self):
+        assert (self.users is not None and len(self.users) > 0)
+        grads = []
+        for param in self.model.parameters():
+            if param.grad is None:
+                grads.append(torch.zeros_like(param.data))
+            else:
+                grads.append(param.grad)
+        for user in self.users:
+            user.set_grads(grads)
+
+    def train(self):
+        loss = []
+        for glob_iter in range(self.num_glob_iters):
+            print("-------------Round number: ",glob_iter, " -------------")
+            # send all parameter for users 
+            self.send_parameters()
+
+            # Evaluate gloal model on user for each interation
+            print("Evaluate global model")
+            print("")
+            self.evaluate()
+            #self.train_evaluate()
+
+            # do update for all users not only selected users
+            for user in self.users:
+                user.train(self.local_epochs) #* user.train_samples
+            
+            # choose several users to send back upated model to server
+            # self.personalized_evaluate()
+
+            self.selected_users = self.select_users(glob_iter,self.num_users)
+
+            # Evaluate gloal model on user for each interation
+            #print("Evaluate persionalized model")
+            #print("")
+            #self.evaluate_personalized_model()
+
+            self.persionalized_aggregate_parameters()
+
+            #loss_ /= self.total_train_samples
+            #loss.append(loss_)
+            #print(loss_)
+
+        #print(loss)
+        self.save_results()
+        self.save_model()
+    
+  
---
+++ fedl/users/userapfl.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+import json
+from torch.utils.data import DataLoader
+from fedl.users.userbase import User
+import copy
+
+class UserAPFL(User):
+    """
+    User in FedAvg.dataset
+    """
+    def __init__(self, numeric_id, train_data, test_data, model, batch_size, learning_rate,meta_learning_rate,lamda,
+                 local_epochs, optimizer):
+        super().__init__(numeric_id, train_data, test_data, model[0], batch_size, learning_rate, meta_learning_rate, lamda,
+                         local_epochs)
+
+        if(model[1] == "cnn" or model[1] == "Mclr_Mnist"):
+            self.loss = nn.NLLLoss()
+        else:
+            self.loss = nn.CrossEntropyLoss()
+        #if optimizer == "SGD":
+        #    self.optimizer = MySGD(self.model.parameters(), lr=self.learning_rate)
+        #if optimizer == "PersionalizedOptimizer":
+        
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.learning_rate, lamda=self.lamda)
+
+    def set_grads(self, new_grads):
+        if isinstance(new_grads, nn.Parameter):
+            for model_grad, new_grad in zip(self.model.parameters(), new_grads):
+                model_grad.data = new_grad.data
+        elif isinstance(new_grads, list):
+            for idx, model_grad in enumerate(self.model.parameters()):
+                model_grad.data = new_grads[idx]
+
+    def train(self, epochs):
+        LOSS = 0
+        self.model.train()
+        for epoch in range(1, self.local_epochs + 1):  # local update 
+            self.model.train()
+            loss_per_epoch = 0
+            #for _ , (X, y) in enumerate(self.trainloader): # ~ t time update
+            try:
+                # Samples a new batch for persionalizing
+                (X, y) = next(self.iter_trainloader)
+            except StopIteration:
+                # restart the generator if the previous generator is exhausted.
+                self.iter_trainloader = iter(self.trainloader)
+                (X, y) = next(self.iter_trainloader)
+
+            K = 3 # K is number of personalized steps
+            for i in range(K):
+                self.optimizer.zero_grad()
+                output = self.model(X)
+                loss = self.loss(output, y)
+                loss.backward()
+                self.persionalized_model, _ = self.optimizer.step(self.local_weight_updated)
+                loss_per_epoch += loss.item() * X.shape[0]
+            loss_per_epoch /= self.train_samples
+            LOSS += loss_per_epoch
+            
+            # update local weight after finding aproximate theta
+            for new_param, localweight in zip(self.persionalized_model, self.local_weight_updated):
+                localweight.data = localweight.data - self.lamda* self.learning_rate * (localweight.data - new_param.data)
+        
+        # evaluate personal model before making argeation at the server size 
+        #test_acc, _ = self.test()
+        #print("check accurancy of peronal model ", test_acc)
+        #self.update_parameters(self.local_weight_updated)
+
+        result = LOSS / self.local_epochs
+        #print(result)
+        return result
