e6aed965e5fc88dfa36b1a659a5df7dfce50d460
CharlieDinh <CharlieSex@156>
2020-06-01 13:11:03 +1000

    Update comments

--- main_plot.py
+++ main_plot.py
@@ -14,28 +14,28 @@ if(1): # plot for MNIST
     numusers = 5
     num_glob_iters = 800
     dataset = "Mnist"
-    local_ep = [20,20,20,20,20,20,20]
+    local_ep = [20,20,20,20]
     lamda = [15,15,15,15]
     learning_rate = [0.005, 0.005, 0.005, 0.005]
-    beta =  [0.001, 0.001, 0.001, 0.001]
+    beta =  [1.0, 1.0, 0.001, 1.0]
     batch_size = [20,20,20,20]
     K = [5,5,5,5,5,5]
-    personal_learning_rate = [0.1,0.1,0.1,0.1,0.1,0.1]
+    personal_learning_rate = [0.1,0.1,0.1,0.1]
     algorithms = [ "pFedMe_p","pFedMe","PerAvg_p","FedAvg"]
     plot_summary_one_figure_mnist_Compare(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=lamda,
                                learning_rate=learning_rate, alpha = beta, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset, k = K, personal_learning_rate = personal_learning_rate)
 
-if(1): # plot for Synthetic
+if(0): # plot for Synthetic
     numusers = 10
     num_glob_iters = 600
     dataset = "Synthetic"
-    local_ep = [20,20,20,20,20,20]
+    local_ep = [20,20,20,20]
     lamda = [20,20,20,20]
-    learning_rate = [0.005, 0.005, 0.005, 0.005, 0.005, 0.005]
-    beta =  [0.001, 0.001, 0.001, 0.001, 0.001, 0.001]
-    batch_size = [20,20,20,20,20,20]
-    K = [5,5,5,5,5,5]
-    personal_learning_rate = [0.01,0.01,0.01,0.01,0.01,0.01] 
+    learning_rate = [0.005, 0.005, 0.005, 0.005]
+    beta =  [1.0, 1.0, 0.001, 1.0]
+    batch_size = [20,20,20,20]
+    K = [5,5,5,5]
+    personal_learning_rate = [0.01,0.01,0.01,0.01] 
     algorithms = [ "pFedMe_p","pFedMe","PerAvg_p","FedAvg"]
     plot_summary_one_figure_synthetic_Compare(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=lamda,
                                learning_rate=learning_rate, alpha = beta, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset, k = K, personal_learning_rate = personal_learning_rate)
