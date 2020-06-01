a1c7dca45b8f04162b3d677184330821ca259ccc
CharlieDinh <CharlieSex@156>
2020-06-01 10:48:27 +1000

    add plot files and remove unnecessary files

---
+++ main_plot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import importlib
+import random
+import os
+from utils.plot_utils import plot_summary_one_figure_mnist_Compare, plot_summary_one_figure_synthetic_Compare
+import torch
+torch.manual_seed(0)
+
+if(1): # plot for MNIST
+    numusers = 5
+    num_glob_iters = 800
+    dataset = "Mnist"
+    local_ep = [20,20,20,20,20,20,20]
+    lamda = [15,15,15,15]
+    learning_rate = [0.005, 0.005, 0.005, 0.005]
+    alpha =  [0.001, 0.001, 0.001, 0.001]
+    batch_size = [20,20,20,20]
+    K = [5,5,5,5,5,5]
+    personal_learning_rate = [0.1,0.1,0.09,0.09,0.09,0.09]
+    algorithms = [ "Persionalized_p","Persionalized","PerAvg_p","FedAvg"]
+    plot_summary_one_figure_mnist_Compare(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=lamda,
+                               learning_rate=learning_rate, alpha = alpha, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset, k = K, personal_learning_rate = personal_learning_rate)
+
+if(1): # plot for Synthetic
+    numusers = 10
+    num_glob_iters = 600
+    dataset = "Synthetic"
+    local_ep = [20,20,20,20,20,20]
+    lamda = [20,20,15,15]
+    learning_rate = [0.005, 0.005, 0.005, 0.005, 0.005, 0.005]
+    alpha =  [0.001, 0.001, 0.001, 0.001, 0.001, 0.001]
+    batch_size = [20,20,20,20,20,20]
+    K = [5,5,5,5,5,5]
+    personal_learning_rate = [0.01,0.01,0.01,0.01,0.01,0.01] 
+    algorithms = [ "Persionalized_p","Persionalized","FedAvg","PerAvg_p"]
+    plot_summary_one_figure_synthetic_Compare(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=lamda,
+                               learning_rate=learning_rate, alpha = alpha, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset, k = K, personal_learning_rate = personal_learning_rate)
+
